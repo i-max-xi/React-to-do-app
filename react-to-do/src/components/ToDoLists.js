e28e6d4b14254f118c7db2eb1774ac06b9b8487c
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-07-05 07:57:13 +0000

    Add input forms

--- react-to-do/src/components/ToDoLists.js
+++ react-to-do/src/components/ToDoLists.js
@@ -1,19 +1,17 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodosList = (props) => {
-    return (
-      <ul>
-        {props.todos.map((todo) => (
-          <TodoItem
-          key={todo.id}
-          todo={todo}
-          handleChangeProps={props.handleChangeProps}
-          deleteTodoProps = {props.deleteTodoProps}
-        />
-        ))}
-      </ul>
-    );
-}
+const TodosList = (props) => (
+    <ul>
+      {props.todos.map((todo) => (
+        <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps = {props.deleteTodoProps}
+      />
+      ))}
+    </ul>
+  );
 
 export default TodosList;
